6dee60226e0b69d4d4805780e3d8167146d62406
Eduardo Gulias Davis <=>
2016-12-02 23:56:34 +0100

    parsing comments

--- src/main/scala/emailvalidator/parser/EmailParser.scala
+++ src/main/scala/emailvalidator/parser/EmailParser.scala
@@ -18,36 +18,33 @@ object EmailParser extends Parsers {
     }
   }
 
-  def local: Parser[Token] = (log(dquote)("dquoute") | log(rep(atom ~> opt(DOT() ~> atom)))("repeat atom") <~ log(opt(escaped))("escaped")) ~> log(comment)("comment") <~ log(AT())("finding @")
+  def local = (log(dquote)("dquoute") | log(rep(atom ~> opt(DOT() ~> atom)))("repeat atom") <~ (log(escaped)("escaped") | log(comment)("comment2")) | Parser{in=>Success(in.first, in.rest)}) <~ log(AT())("finding @")
 
   def atom = acceptIf {
     case _: GENERIC => true
     case _ => false
   }(t => s"failed at $t")
 
-  def comment: Parser[Token] =
-    opt(OPENPARENTHESIS()) into(op => op match {
-      case Some(x) => rep(log(atom)("comment atom")) ~> CLOSEPARENTHESIS()
-      case None => Parser{ in => Success(null, in) }
-    })
+
+  def comment: Parser[Token] = OPENPARENTHESIS() ~> (atom | comment) <~ CLOSEPARENTHESIS()
 
   def dquote: Parser[Token] = log(DQUOTE())("open quote") ~> opt(rep(log(atom)("double quote atom"))) ~> log(DQUOTE())("closing quote")
 
   def escaped = log(BACKSLASH())("escaped backslash") ~ log(consumeTokenNot(atom))("not atom") ~> atom
 
-  private def consumeTokenNot [T](p: => Parser[T]): Parser[Unit] = Parser { in =>
+  private def consumeTokenNot[T](p: => Parser[T]): Parser[Unit] = Parser { in =>
     p(in) match {
-      case Success(_, _)  => Failure("Expected failure", in)
-      case _              => Success(in.first, in.rest)
+      case Success(_, _) => Failure("Expected failure", in)
+      case _ => Success(in.first, in.rest)
     }
   }
 
   def domain = phrase(log(domainAtom)("domain atom") ~> log(rep(DOT() ~> domainAtom))("repeat domain atom"))
 
   def domainAtom = acceptIf {
-      case c: GENERIC => c.isAscii
-      case _ => false
-    }(t => s"failed at $t")
+    case c: GENERIC => c.isAscii
+    case _ => false
+  }(t => s"failed at $t")
 
   def hasAt: Parser[Token] = (tr: TokenReader) => {
     if (tr.tokenizedSource.contains(AT())) Success(tr.first, tr.rest)
