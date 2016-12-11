f58e1d30844f3af1d410f2413cb30b13a8867cb8
Eduardo Gulias Davis <=>
2016-12-11 16:22:56 +0100

    Removed logs

--- src/main/scala/emailvalidator/parser/EmailParser.scala
+++ src/main/scala/emailvalidator/parser/EmailParser.scala
@@ -9,7 +9,8 @@ import scala.util.parsing.combinator.Parsers
 object EmailParser extends Parsers {
   type Elem = Token
 
-  def local = (log(dquote)("dquoute") | log(rep(atom ~> opt(DOT <~ atom )))("repeat atom") <~ (log(comment)("comment2") | Parser{in=>Success(in.first, in)})) ~> log(AT)("finding @")
+  def local:Parser[Token] =
+    (dquote | rep(atom ~> opt(DOT <~ atom )) <~ (comment | Parser{in=>Success(in.first, in)})) ~> AT
 
   private def atom = acceptIf {
     case _: GENERIC => true
@@ -19,13 +20,13 @@ object EmailParser extends Parsers {
 
   private def comment: Parser[Token] = OPENPARENTHESIS ~> (atom | comment) <~ CLOSEPARENTHESIS ~ opt(SPACE)
 
-  private def dquote = log(DQUOTE)("open quote") ~> commit(rep(log(dquoteAtom)("double quote atom")) <~ log(DQUOTE)("closing quote"))
+  private def dquote = DQUOTE ~> commit(rep(dquoteAtom) <~ DQUOTE)
 
   private def dquoteAtom:Parser[Token] = atom | COMMA | AT | SPACE | (BACKSLASH ~> DQUOTE) | BACKSLASH
 
-  def domain = phrase(log(literalDomain)("litdom") | (log(rep1(domainAtom))("domain atom") ~ log(rep(DOT ~> domainAtom))("repeat domain atom")))
+  def domain:Parser[Object] = phrase(literalDomain | (rep1(domainAtom) ~ rep(DOT ~> domainAtom)))
 
-  private def literalDomain = OPENBRACKET ~> (log(IPv6)("IPv6") | log(IPv4)("IPv4")) <~ CLOSEBRACKET
+  private def literalDomain = OPENBRACKET ~> (IPv6 | IPv4) <~ CLOSEBRACKET
 
   private def IPv6 = IPV6TAG ~ (COLON ~ opt(COLON)) ~ rep(opt(IPv6part) ~ COLON) into {
     case _ ~ (COLON~Some(_)) ~ xs =>
@@ -51,7 +52,7 @@ object EmailParser extends Parsers {
     case _ => false
   }(t => s"failed at $t")
 
-  private def IPv4 = log(repsep(IPv4part, DOT))("repsep") into {
+  private def IPv4 = repsep(IPv4part, DOT) into {
     case Nil => Parser[Token] {in => Success(in.first, in)}
     case xs if xs.length > 4 => Parser[Token] {in => Failure("More than 4 elements in IPv4", in)}
     case _ => Parser[Token] {in => Success(in.first, in)}
