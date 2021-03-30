81c6dd38d0bf29dd3d8800785557c46ccfdb20de
Eduardo Gulias Davis <eduardomgulias@gmail.com>
2021-03-31 00:15:47 +0200

    cleanup

--- src/test/scala/emailvalidator/EmailValidatorSpec.scala
+++ src/test/scala/emailvalidator/EmailValidatorSpec.scala
@@ -1,47 +1,43 @@
 package emailvalidator
 
-import org.scalatest.FunSpec
+import org.scalatest.funsuite.AnyFunSuite
 
-class EmailValidatorSpec extends FunSpec {
-  describe("Validates emails") {
-    it("should return success for a valid email") {
-
+class EmailValidatorSpec extends AnyFunSuite {
+  test("Validates emails") {
       assert(EmailValidator.validate("example@example").isRight)
-    }
+  }
+  test("should return failure for an invalid email") {
+    val emails = List(
+          "@example.co.uk",
+          "example@",
+          "example@example-.co.uk",
+          "example@example-",
+          "example@@example.co.uk",
+          "example..example@example.co.uk",
+          "example@example..co.uk",
+          "<example_example>@example.fr",
+          ".example@localhost",
+          "example@.localhost",
+          "example@localhost.",
+          "example.@example.co.uk",
+          "(example@localhost",
+          """"example@localhost""",
+          """"exa"mple@localhost""",
+          "(example@localhost",
+          "comment)example@localhost",
+          "example(comment))@localhost",
+          "example@comment)localhost",
+          "example@localhost(comment))",
+          "example@(comment))example.com",
+          "exampl\ne@example.co.uk",
+          "example@[[]",
+          "exampl\te@example.co.uk",
+          "example@exa\rmple.co.uk",
+          "example@[\r]",
+          "exam\rple@example.co.uk")
 
-    it("should return failure for an invalid email") {
-      val emails = List(
-            "@example.co.uk",
-            "example@",
-            "example@example-.co.uk",
-            "example@example-",
-            "example@@example.co.uk",
-            "example..example@example.co.uk",
-            "example@example..co.uk",
-            "<example_example>@example.fr",
-            ".example@localhost",
-            "example@.localhost",
-            "example@localhost.",
-            "example.@example.co.uk",
-            "(example@localhost",
-            """"example@localhost""",
-            """"exa"mple@localhost""",
-            "(example@localhost",
-            "comment)example@localhost",
-            "example(comment))@localhost",
-            "example@comment)localhost",
-            "example@localhost(comment))",
-            "example@(comment))example.com",
-            "exampl\ne@example.co.uk",
-            "example@[[]",
-            "exampl\te@example.co.uk",
-            "example@exa\rmple.co.uk",
-            "example@[\r]",
-            "exam\rple@example.co.uk")
-
-      for {
-        email <- emails
-      } yield assert(EmailValidator.validate(email).isLeft, email)
-    }
+    for {
+      email <- emails
+    } yield assert(EmailValidator.validate(email).isLeft, email)
   }
 }
--- src/test/scala/emailvalidator/lexer/TokenizerSpec.scala
+++ src/test/scala/emailvalidator/lexer/TokenizerSpec.scala
@@ -1,38 +1,36 @@
 package emailvalidator.lexer
 
-import org.scalatest.FunSpec
+import org.scalatest.funsuite.AnyFunSuite
 
-class TokenizerSpec extends FunSpec{
-  describe ("A Tokenizer") {
-    it("should produce special tokens from a special string") {
-      val expected = Token.special
-      for (t <- expected) assert(List(t._2) === Tokenizer.tokenize(t._1))
-    }
+class TokenizerSpec extends AnyFunSuite {
+  test("should produce special tokens from a special string") {
+    val expected = Token.special
+    for (t <- expected) assert(List(t._2) == Tokenizer.tokenize(t._1))
+  }
 
-    it("should produce generic tokens for non special") {
-       assert(List(GENERIC("test")) === Tokenizer.tokenize("test"))
-    }
+  test("should produce generic tokens for non special") {
+     assert(List(GENERIC("test")) == Tokenizer.tokenize("test"))
+  }
 
-    it("should generate multiple types of tokens for a complex string") {
-      val complexString = "A string with: Multiple\\ [t@kens]"
-      val expected = List(
-        GENERIC("A"),
-        SPACE,
-        GENERIC("string"),
-        SPACE,
-        GENERIC("with"),
-        COLON,
-        SPACE,
-        GENERIC("Multiple"),
-        BACKSLASH,
-        SPACE,
-        OPENBRACKET,
-        GENERIC("t"),
-        AT,
-        GENERIC("kens"),
-        CLOSEBRACKET
-      )
-      assert(expected === Tokenizer.tokenize(complexString))
-    }
+  test("should generate multiple types of tokens for a complex string") {
+    val complexString = "A string with: Multiple\\ [t@kens]"
+    val expected = List(
+      GENERIC("A"),
+      SPACE,
+      GENERIC("string"),
+      SPACE,
+      GENERIC("with"),
+      COLON,
+      SPACE,
+      GENERIC("Multiple"),
+      BACKSLASH,
+      SPACE,
+      OPENBRACKET,
+      GENERIC("t"),
+      AT,
+      GENERIC("kens"),
+      CLOSEBRACKET
+    )
+    assert(expected == Tokenizer.tokenize(complexString))
   }
 }
